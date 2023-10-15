2bc48fac13bbd4ed08ed83df0ac294ee1b40d314
Nguyen Minh Tuan <tnowad@gmail.com>
2023-10-15 20:20:06 +0700

    feat(qtile): change string to number group

--- .config/qtile/core/groups.py
+++ .config/qtile/core/groups.py
@@ -7,19 +7,19 @@ from utils.match import wm_class
 groups: list[Group] = []
 
 for key, label, layout, matches in [
-    ("1", "", None, wm_class("wezterm")),
-    ("2", "", "max", wm_class("code")),
+    ("1", "", None, wm_class("kitty")),
+    ("2", "", "max", wm_class("kitty", "code", "android-studio")),
     ("3", "", None, wm_class("insomnia", "obs", "evince")),
-    ("q", "󰈹", "max", wm_class("brave-browser", "firefox")),
-    ("w", "󰇮", "max", wm_class("discord", "telegram-desktop")),
-    ("f", "", "max", wm_class("spotify", "vlc")),
+    ("4", "󰈹", "max", wm_class("google-chrome-stable", "firefox")),
+    ("5", "󰇮", "max", wm_class("discord", "telegram-desktop")),
+    ("6", "", "max", wm_class("spotify", "vlc")),
 ]:
     groups.append(Group(key, matches, label=label, layout=layout))
 
     keys.extend([
         # mod1 + letter of group = switch to group
-        Key([mod], key, lazy.group[key].toscreen(toggle=True)),
+        Key([mod], key, lazy.group[key].toscreen(toggle=False)),
 
         # mod1 + shift + letter of group = move focused window to group
-        Key([mod, "shift"], key, lazy.window.togroup(key)),
+        Key([mod, "shift"], key, lazy.window.togroup(key, switch_group=True)),
     ])  # fmt: skip
