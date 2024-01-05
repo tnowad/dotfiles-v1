5ef006bba97732ef4df9c0496c96212cffbe3a29
Nguyen Minh Tuan <tnowad@gmail.com>
2024-01-06 05:11:16 +0700

    feat(script): remove unnecessary echo

--- install.sh
+++ install.sh
@@ -8,9 +8,7 @@ for dir in "$DOTFILES_DIR"/*/; do
 	if [ -d "$dir" ]; then
 		install_script="$dir/install.sh"
 		if [ -f "$install_script" ]; then
-			echo "Installing dotfiles from $(basename "$dir"): running $install_script"
 			(cd "$dir" && ./install.sh)
-			echo "Installation completed for $(basename "$dir")."
 		else
 			echo "No install script found for $(basename "$dir"), skipping..."
 		fi
