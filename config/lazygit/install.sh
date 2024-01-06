4151e5e84dd1eca6fa8038789ab50c89545198ef
Nguyen Minh Tuan <tnowad@gmail.com>
2024-01-07 05:55:33 +0700

    feat(script): add script install for lazygit

---
+++ config/lazygit/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# shellcheck disable=1091,2153
+
+source "$SCRIPTS_DIR/link_config.sh"
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+SOURCE_DIR="$SCRIPT_DIR/config"
+CONFIG_DIR="$HOME/.config/lazygit"
+
+link_config "$SOURCE_DIR" "$CONFIG_DIR"
+
+echo "Picom configuration linked."
