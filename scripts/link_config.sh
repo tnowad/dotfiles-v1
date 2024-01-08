c4a658b8ef798a89ff8b152c614578ac67a3d501
Nguyen Minh Tuan <tnowad@gmail.com>
2024-01-08 10:12:14 +0700

    refactor(script): enhance `link_config` for file and directory linking

--- scripts/link_config.sh
+++ scripts/link_config.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
 link_config() {
-	local source_dir=$1
-	local config_dir=$2
+	local source=$1
+	local destination=$2
 
-	echo "Linking: $source_dir -> $config_dir"
-	if [ -e "$config_dir" ] || [ -L "$config_dir" ]; then
-		if [ "$(readlink -- "$config_dir")" = "$source_dir" ]; then
-			echo "The link already exists and points to the correct directory."
+	if [ -d "$source" ]; then
+		echo "Linking directory: $source -> $destination"
+	elif [ -f "$source" ]; then
+		echo "Linking file: $source -> $destination"
+	else
+		echo "Error: Source is neither a file nor a directory."
+		return 1
+	fi
+
+	if [ -e "$destination" ] || [ -L "$destination" ]; then
+		if [ "$(readlink -- "$destination")" = "$source" ]; then
+			echo "The link already exists and points to the correct location."
 			return 0
 		fi
-		read -p "The destination directory already exists. Do you want to override it? (y/n): " -r override
+
+		read -p "The destination already exists. Do you want to override it? (y/n): " -r override
 		if [ "$override" != "y" ]; then
 			echo "Linking aborted."
 			return 1
 		fi
-		rm -rf "$config_dir"
+
+		rm -rf "$destination"
 	fi
 
-	ln -s "$source_dir" "$config_dir"
-	echo "Configuration linked."
+	ln -s "$source" "$destination"
+	echo "Link created successfully."
 }
